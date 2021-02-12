ff37b01c29f48f2b9cd11d96c434e21fa028cb7b
Morris-Desktop <morteza.hazrati@gmail.com>
2021-02-12 11:36:57 +1100

    The root App converted to a class based react component. The state initialized to have all the unique color by executing the createColors helper function

--- src/App.js
+++ src/App.js
@@ -3,37 +3,40 @@ import Pixel from "./components/pixel";
 import { createColors } from "./services/createColors";
 import "./App.css";
 
-function App() {
-  let w = 100;
-  const COLORS = createColors();
-  console.log(COLORS[0]);
-  console.log(COLORS[32767]);
+class App extends Component {
+  state = { colors: createColors() };
+  render() {
+    let w = 100;
 
-  const formatColor = (colorObject) => {
-    let rFactor = colorObject.r;
-    let gFactor = colorObject.g;
-    let bFactor = colorObject.b;
+    console.log(this.state.colors[0]);
+    console.log(this.state.colors[32767]);
 
-    return `rgb(${rFactor}, ${gFactor}, ${bFactor})`;
-  };
+    const formatColor = (colorObject) => {
+      let rFactor = colorObject.r;
+      let gFactor = colorObject.g;
+      let bFactor = colorObject.b;
 
-  return (
-    <div
-      style={{
-        width: 400,
-        hieght: 500,
-        backgroundColor: "yellow",
-        display: "flex",
-        flexWrap: "wrap",
-      }}
-    >
-      <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
-      <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
-      <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
-      <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
-      <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
-    </div>
-  );
+      return `rgb(${rFactor}, ${gFactor}, ${bFactor})`;
+    };
+
+    return (
+      <div
+        style={{
+          width: 400,
+          hieght: 500,
+          backgroundColor: "yellow",
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
+        <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
+        <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
+        <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
+        <Pixel color={formatColor({ r: 255, g: 0, b: 0 })} />
+      </div>
+    );
+  }
 }
 
 export default App;
